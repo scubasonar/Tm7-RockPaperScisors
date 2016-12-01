3947f761c6cd80f88e7c6b2edd2dfa7de9de633a
k-vader <kvader@csumb.edu>
2016-11-30 21:38:50 -0800

    Added Player class to track player info during a session

--- RockPaperScissors/src/main/java/cst438/RPSGame.java
+++ RockPaperScissors/src/main/java/cst438/RPSGame.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RPSGame extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	public enum Value {Rock, Paper, Scissors}
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,4 +37,71 @@ public class RPSGame extends HttpServlet {
 		doGet(request, response);
 	}
 
+}
+
+/**
+ * Player class to track player info during a session
+ */
+class Player
+{
+	private int won;
+	private int lost;
+	private RPSGame.Value value;
+	
+	public Player()
+	{
+		setWon(0);
+		setLost(0);
+		setValue(RPSGame.Value.Rock);
+	}
+	
+	public RPSGame.Value getValue()
+	{
+		return value;
+	}
+	
+	public void setValue(RPSGame.Value value)
+	{
+		this.value = value;
+	}
+	
+	public int getLost()
+	{
+		return lost;
+	}
+	
+	public void incrementLost()
+	{
+		lost++;
+	}
+	
+	public void setLost(int lost)
+	{
+		if (lost < 0)
+		{
+			return;
+		}
+		
+		this.lost = lost;
+	}
+	
+	public int getWon()
+	{
+		return won;
+	}
+	
+	public void incrementWon()
+	{
+		won++;
+	}
+	
+	public void setWon(int won)
+	{
+		if (won < 0)
+		{
+			return;
+		}
+		
+		this.won = won;
+	}
 }
