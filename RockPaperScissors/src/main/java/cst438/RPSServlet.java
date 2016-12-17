c30b309dfd3bdff1c10db00917ff88c1263f76f7
Matt Valancy <scubasonar@gmail.com>
2016-12-16 20:12:09 -0800

    get request sends you to index.jsp instead of an empty page

--- RockPaperScissors/src/main/java/cst438/RPSServlet.java
+++ RockPaperScissors/src/main/java/cst438/RPSServlet.java
@@ -36,7 +36,7 @@ public class RPSServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
