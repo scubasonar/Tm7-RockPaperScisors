f1f03f52ff66291fb5e4ae8ea8a09456e3fb39f3
Matt Valancy <scubasonar@gmail.com>
2016-12-16 22:27:04 -0800

    Added on to game state and game settings

--- RockPaperScissors/src/main/java/cst438/GameSettings.java
+++ RockPaperScissors/src/main/java/cst438/GameSettings.java
@@ -2,10 +2,27 @@ package cst438;
 
 public class GameSettings {
 
-	private int _max_rounds = 3;
+	private int _maxRounds = 3;
+	private int _halfWayPoint = 1; // get more wins or losses than this to win the game
 	
 	public GameSettings(int max_rounds)
 	{
-		_max_rounds = max_rounds;
+		_maxRounds = max_rounds;
+	}
+	
+	public int GetMaxRounds()
+	{
+		return _maxRounds;
+	}
+	
+	public int GetHalfWay()
+	{
+		return _halfWayPoint;
+	}
+	
+	public void SetMaxRounds(int rounds)
+	{
+		_halfWayPoint = rounds / 2;
+		_maxRounds = rounds;
 	}
 }
--- RockPaperScissors/src/main/java/cst438/GameState.java
+++ RockPaperScissors/src/main/java/cst438/GameState.java
@@ -4,10 +4,39 @@ import java.util.List;
 
 public class GameState 
 {
-	Player _player;
+	enum GameStatus {PLAY, WIN, LOSE, TIE};
 	
-	public GameState( Player player)
+	// we track rounds won separate from games won. Games won gets saved with a player but games can 
+	// be more than one round. 
+	private int _roundsWon;
+	private int _roundsLost;
+	private int _roundsDraw;
+	
+	private GameStatus _gameStatus = GameStatus.PLAY;
+	private GameStatus _LastRoundStatus = GameStatus.LOSE;
+	
+	public GameState()
 	{
-			_player = player;
+			
+	}
+	
+	public int getRoundsWon()
+	{
+		return _roundsWon;
+	}
+	
+	public int getRoundsLost()
+	{
+		return _roundsLost;
+	}
+	
+	public GameStatus getGameStatus()
+	{
+		return _gameStatus;
+	}
+	
+	public void setGameStatus(GameStatus status)
+	{
+		_gameStatus = status;
 	}
 }
